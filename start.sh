a758cc5f3c80c0f5807b2f3bbe2e8b828ada6b19
Rahul <87630115+XTelegram@users.noreply.github.com>
2022-07-25 22:05:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,14 +11,14 @@
 ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 ## Adding Files ----
 
-if [[ -n $CONFIG_ENV_URL ]]; then
-  echo " Found config.env File 📁📁"
-	wget -q $CONFIG_ENV_URL -O /app/config.env
-fi
-
-if [ -f .env ] ; then  set -o allexport; source .env; set +o allexport ; fi
+python3 update.py && python3 -m bot
 
 
-echo "Starting Your Tele-LeechX... ♻️♻️"
-python3 update.py && python3 -m tobrot
+
+
+
+
+
+
+
 
